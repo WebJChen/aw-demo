5dfc4125a815820257b540d91d934a42cf2bd53b
karinac <2737185110@qq.com>
2026-06-04 00:00:44 +0800

    优化了搜索和搜索定位功能

---
+++ assets/tasmaniaGridStyleTestThumbs-CI7ZZCg_.js
@@ -0,0 +1,1 @@
+import{bA as n,bB as o}from"./index-Jm2izSea.js";const e=Object.freeze([n,o]);function T(r){const t=Number(r),s=Number.isFinite(t)&&t>=0?Math.floor(t):0;return e[s%e.length]}export{T as t};
