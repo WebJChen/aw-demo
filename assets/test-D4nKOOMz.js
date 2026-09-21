d29efbbc1d2398ef935d5de419cfc8981e2bfd10
Kurinabluu <Kurinabluu@users.noreply.github.com>
2026-09-21 12:33:25 +0000

    deploy: d00034f7af65023e436b1dac50e3d67d932f13b8

---
+++ assets/test-D4nKOOMz.js
@@ -0,0 +1,1 @@
+import{_ as t,d as e,y as s}from"./index-C8lU8BqK.js";const c={},o={class:"box"};function _(a,n){return s(),e("div",o,"test text")}const d=t(c,[["render",_],["__scopeId","data-v-ba278378"]]);export{d as default};
